d8f37ad4a5590ebe992d172afe3b0e3e28aced5c
max <max@draminski.com>
2024-12-24 14:09:06 +0100

    add log in native code

--- app/src/main/cpp/library1.c
+++ app/src/main/cpp/library1.c
@@ -1,7 +1,14 @@
 #include <jni.h>
 #include <stdio.h>
+#include <android/log.h> // Dodaj ten nagłówek
+
+#define LOG_TAG "NativeCallbackTest" // Ustaw nazwę tagu dla logów
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__) // Log poziomu INFO
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__) // Log poziomu ERROR
 
 void library1_callback(const char* message) {
+    LOGI("!!!!!!!!!!!!!!!!!! LIBRARY 1 ");
+
     printf("Library1 callback: %s\n", message);
 }
 
--- app/src/main/cpp/library2.c
+++ app/src/main/cpp/library2.c
@@ -1,5 +1,10 @@
 #include <jni.h>
 #include <stdio.h>
+#include <android/log.h> // Dodaj ten nagłówek
+
+#define LOG_TAG "NativeCallbackTest" // Ustaw nazwę tagu dla logów
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__) // Log poziomu INFO
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__) // Log poziomu ERROR
 
 typedef void (*Callback)(const char*);
 
@@ -9,13 +14,17 @@ static Callback registered_callback = NULL;
 JNIEXPORT void JNICALL
 Java_com_example_nativecallbacktest_NativeInterop_registerCallbackInLibrary2(JNIEnv *env, jobject obj, jlong functionPointer) {
     registered_callback = (Callback)functionPointer;
+    LOGI("Callback registered successfully.");
 }
 
 JNIEXPORT void JNICALL
 Java_com_example_nativecallbacktest_NativeInterop_triggerCallback(JNIEnv *env, jobject obj) {
+
+    LOGI("!!!!!!!!!!!!!!!!!! LIBRARY 2 ");
     if (registered_callback != NULL) {
         registered_callback("Hello from Library2!");
+        LOGI("Callback version");
     } else {
-        printf("No callback registered.\n");
+        LOGI("No callback registered.\n");
     }
 }
